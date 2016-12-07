b1fa7b4485708d07a9f8a8d3836a094341f68c49
vcburnett <victorburnett@Victors-iMac-2.local>
2016-12-06 21:09:16 -0500

    Case Study and styles

--- case-study.php
+++ case-study.php
@@ -25,11 +25,49 @@
 			<div id="cs-img-header" style="background-image: url('assets/images/portfolio/portfolio-header-shi.jpg');">
 				<div class="cs-img-content-wrapper">
 					<h1>Sustainable Harvest International</h1>
-					<a href="#the-content" class="scroll">Read the case study</a>
+					<a href="#the-content" class="scroll">
+						Read the case study
+						<span class="arrow">
+							<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 130.31 75.15">
+								<path d="M65.15,75.15a10,10,0,0,1-7.07-2.93L2.93,17.07A10,10,0,0,1,17.07,2.93L65.15,51,113.24,2.93a10,10,0,0,1,14.14,14.14L72.23,72.23A10,10,0,0,1,65.15,75.15Z"/>
+							</svg>
+						</span>
+					</a>
+				</div>
+				<div class="cs-services">
+					<div class="cs-service-icon-wrap">
+						<div class="cs-service-icon">
+							<img src="assets/images/icon-consulting.svg" alt="Consulting">
+						</div>
+						<h2>Consulting</h2>
+					</div>
+					<div class="cs-service-icon-wrap">
+						<div class="cs-service-icon">
+							<img src="assets/images/icon-web-analytics.svg" alt="Web Analytics">
+						</div>
+						<h2>Web Analytics</h2>
+					</div>
+					<div class="cs-service-icon-wrap">
+						<div class="cs-service-icon">
+							<img src="assets/images/icon-sem-seo.svg" alt="SEM & SEO">
+						</div>
+						<h2>SEM & SEO</h2>
+					</div>
+					<div class="cs-service-icon-wrap">
+						<div class="cs-service-icon">
+							<img src="assets/images/icon-ux-design.svg" alt="UX Design">
+						</div>
+						<h2>UX Design</h2>
+					</div>
+					<div class="cs-service-icon-wrap">
+						<div class="cs-service-icon">
+							<img src="assets/images/icon-design.svg" alt="Web Design">
+						</div>
+						<h2>Web Design</h2>
+					</div>
 				</div>
 			</div>
 			<div class="internal-wrapper" id="the-content">
-				<p>UX, Web, SEM, Consulting</p>
 				<p>Bacon ipsum dolor amet jowl pancetta ribeye, brisket landjaeger picanha chuck. Boudin porchetta beef bacon short loin. Landjaeger drumstick corned beef cow, short loin pastrami shoulder doner andouille t-bone kevin frankfurter leberkas. Leberkas short loin short ribs, cupim fatback ground round kielbasa shankle.</p>
 				<p>Cupim sirloin chicken kevin ham hock bresaola. Landjaeger jerky filet mignon flank, beef ribs corned beef brisket cow salami capicola ham hock pig. Jowl meatball jerky pork chop tail picanha. Strip steak hamburger ham hock kielbasa ground round. Turducken rump ribeye biltong chicken, turkey shoulder beef ground round.</p>
 			</div>
